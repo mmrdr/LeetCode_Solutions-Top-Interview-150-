48f7abb2eac3586182c47d4507583122f8da5d51
Kirill Isaev <138990956+mmrdr@users.noreply.github.com>
2024-10-16 17:57:08 +0300

    Remove Element solved

---
+++ LeetCode(Top150)/ArrayOrString/RemoveElement.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                ++k;
+            }
+        }
+        return k;
+    }
+};
+
+int main() {
+}
